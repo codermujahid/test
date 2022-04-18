5250687a2d4b2af22f1d3a19eff467542b2de082
codermujahid <developermujahid35@gmail.com>
2022-04-18 12:36:02 +0600

    upload

---
+++ app.js
@@ -0,0 +1,79 @@
+// console.log(20000);
+// console.warn('Asha tumi valo hoy jao');
+// console.error('Tor sob ki su sessss');
+// console.dir('5000')
+// console.info('slalam kamal jobbar')
+
+
+// alert('Type your name');
+
+
+
+// console.log(alert('Type your name'));
+
+// console.log(confirm(' Apni ki mash khaben'));
+
+
+// console.log(prompt('pnar Girl frind er nam ki'));
+
+// let name = prompt('Name')
+// let age = prompt('age')
+// let rol = prompt('Roll')
+// let sub = prompt('Subject')
+// let bn = prompt('Bangla')
+// let en = prompt('English')
+// let math = prompt('Math')
+// let ict = prompt('ICT')
+
+// console.log(`
+
+// Name :      ${name}
+// Age :       ${age}
+// Roll :      ${rol}
+
+// Subject :   ${sub}
+// bangla :   ${bn}
+// English :   ${en}
+// Math :    ${math}
+// ICT :     ${ict}
+
+// `);
+
+// let result = bn + en + math + ict;
+
+// var stu_name = 'mujahid';
+
+// console.log('stu_name');
+
+// Deta typeee
+
+// var name = ('string')
+// var number = (1234)
+
+
+// console.log(name);
+// console.log(number);
+// console.log(confirm('Tmi ar amy ager moto ki valo basho??'));
+
+
+
+// console.log('Golap',  + 123  + 'shapla');
+
+
+
+// concatenation system
+
+let name = "Mujahidul islam"
+let age = 12
+let skill = "JAn go developper"
+
+
+console.log("My name is "+ name+" & I am "+ age +" years old & I am a" + skill);
+
+console.log(`my name is ${name} & I am ${age} years old & I am ${skill}`);
+
+/**
+ * 
+ * 
+ * 
+ */
